839e04403e2c02d08bdef0e12ffde074eac4f151
ZaheedaT <zaheedatshankie@gmail.com>
2026-09-20 15:35:53 +0200

    Adding CI env variables-6

--- model/house_model.py
+++ model/house_model.py
@@ -1,4 +1,5 @@
 import os
+import boto3
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, make_scorer, r2_score
 from sklearn.model_selection import train_test_split, GridSearchCV
